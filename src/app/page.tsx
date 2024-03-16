a87633600ec7b8e4ec3b2c224be1fa46a453a88b
nazal.iqbal@codecraft.co.in <nazal.iqbal@codecraft.co.in>
2024-03-16 13:48:52 +0530

    sdsdss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,9 +73,9 @@ export default function Home() {
             <li
               key={item.id}
               className="relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
-              before:bottom-0 before:left-0 before:bg-white
-              before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
-              before:transition before:ease-in-out before:duration-300"
+    before:bottom-0 before:left-0 before:bg-white
+    before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
+    before:transition before:ease-in-out before:duration-300"
             >
               <Link href={item.href}>{item.text}</Link>
             </li>

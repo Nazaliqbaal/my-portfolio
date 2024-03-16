205ddbccdcec13b132ecd61da81a91af93d2c7f9
nazal.iqbal@codecraft.co.in <nazal.iqbal@codecraft.co.in>
2024-03-16 12:52:21 +0530

    sdsdss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 module.exports = {
-  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
